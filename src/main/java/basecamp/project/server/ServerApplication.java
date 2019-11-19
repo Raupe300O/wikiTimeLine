08a205700df5cfc83783054e112f71d1cb8e67cd
Eugen R <studium@euge.de>
2019-11-19 16:41:12 +0100

    content negotiation deferred

--- src/main/java/basecamp/project/server/ServerApplication.java
+++ src/main/java/basecamp/project/server/ServerApplication.java
@@ -1,6 +1,5 @@
 package basecamp.project.server;
 
-import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -8,7 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @EnableAutoConfiguration
@@ -27,25 +25,6 @@ public class ServerApplication {
 
 		return "index";
 	}
-
-
-
-	// call http://<host>/hello.json
-	@RequestMapping(value = "/hello", method = RequestMethod.GET, produces = "application/json")
-	@ResponseBody
-	public Object helloRest() {
-		JSONObject j = new JSONObject();
-		j.put("hello", "world");
-		return j;
-	}
-
-	// call http://<host>/hello.html or just http://<host>/hello
-	@RequestMapping(value = "/hello", method = RequestMethod.GET)
-	public String helloHtml(Model model) {
-		model.addAttribute("message", "hello world");
-		return "hello";
-	}
-
 
 
 	public static void main(String[] args) {
